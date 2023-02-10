5a27d75e57cf492e245bfb6e4e02af705ceac51b
Steven vanZyl <rushsteve1@rushsteve1.us>
2023-02-10 10:55:35 -0500

    Force the timezone to UTC-5

--- src/jobs.rs
+++ src/jobs.rs
@@ -1,14 +1,16 @@
 use crate::web::{post_random_to_channel, post_stonks_to_channel};
+use chrono::FixedOffset;
 use clokwerk::Interval;
 use clokwerk::{AsyncScheduler, Job, TimeUnits};
 
 use crate::{QOTD_CHANNELS, STONKS_CHANNELS};
 
 pub fn setup_jobs() -> AsyncScheduler {
-    let mut scheduler = AsyncScheduler::new();
+    let mut scheduler = AsyncScheduler::with_tz(FixedOffset::east_opt(5 * 3600).unwrap());
 
     // Quote of the Day schedule
     scheduler.every(1.day()).at("5:00 am").run(|| async {
+        warn!("Running QOTD Job");
         for chan in QOTD_CHANNELS.iter() {
             post_random_to_channel(*chan, "**Quote of the Day**".to_string())
                 .await
@@ -21,6 +23,7 @@ pub fn setup_jobs() -> AsyncScheduler {
         .every(Interval::Weekday)
         .at("5:00 pm")
         .run(|| async {
+            warn!("Running Stonks Job");
             for chan in STONKS_CHANNELS.iter() {
                 post_stonks_to_channel(*chan)
                     .await
