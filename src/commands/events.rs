15ce862fe8fb726d7605c55afdee7e853a8ec6e2
Steven vanZyl <rushsteve1@rushsteve1.us>
2025-07-16 13:58:50 -0400

    Upgrade version to 3.2.0 and add join voice announcements

--- src/commands/events.rs
+++ src/commands/events.rs
@@ -1,5 +1,7 @@
-use poise::serenity_prelude::{self as serenity, ReactionType};
-use tracing::instrument;
+use anyhow::anyhow;
+use poise::serenity_prelude::{self as serenity, CreateMessage, Mentionable, ReactionType};
+use rand::prelude::IndexedRandom;
+use tracing::{info, instrument};
 
 #[instrument]
 pub async fn handler(
@@ -11,6 +13,9 @@ pub async fn handler(
 	match event {
 		serenity::FullEvent::ReactionAdd { add_reaction } => {
 			reaction_handler(ctx, add_reaction).await
+		}
+		serenity::FullEvent::VoiceStateUpdate { old, new } => {
+			voice_state_handler(ctx, old, new).await
 		}
 		_ => Ok(()),
 	}
@@ -46,6 +51,79 @@ async fn reaction_handler(
 			}
 		}
 	}
+
+	Ok(())
+}
+
+const CALLOUTS: &[&str] = &[
+	"joined",
+	"hopped in",
+	"hopped on",
+	"sidled over to",
+	"wandered over to",
+	"connected to",
+	"hit the button on",
+	"jumped into",
+	"crashed",
+	"landed on",
+	"activated",
+	"wants to hang in",
+	"cordially invites you to",
+	"has become a member of",
+	"sat down at",
+	"mounted",
+	"awaits your presence in",
+];
+
+const RADON_ID: u64 = 741129884470083634;
+const FRIENDSHIP_ID: u64 = 1395071978838167623;
+async fn voice_state_handler(
+	ctx: &serenity::Context,
+	old: &Option<serenity::VoiceState>,
+	new: &serenity::VoiceState,
+) -> anyhow::Result<()> {
+	// Don't repeatedly send messages
+	if let Some(old) = old {
+		if old.user_id == new.user_id {
+			return Ok(());
+		}
+	}
+
+	// Get the voice channel
+	let channel = serenity::ChannelId::from(new.channel_id.ok_or(anyhow!("Channel ID not found"))?);
+	let channels = new
+		.guild_id
+		.ok_or(anyhow!("Guild ID not found"))?
+		.channels(ctx)
+		.await?;
+	let voice_channel = channels.get(&channel).ok_or(anyhow!("Channel not found"))?;
+
+	// Only ping if someone joins an empty channel
+	if voice_channel.members(ctx.cache.clone())?.len() > 1 {
+		return Ok(());
+	}
+
+	let member = new.member.clone().ok_or(anyhow::anyhow!("No member"))?;
+
+	// Ignore them if they join deafened
+	if new.self_deaf {
+		return Ok(());
+	}
+
+	// Send the message to the correct channel
+	let radon = serenity::ChannelId::from(RADON_ID);
+	let builder = CreateMessage::new().content(format!(
+		"{} {} {} {}",
+		serenity::RoleId::from(FRIENDSHIP_ID).mention(),
+		member.mention(),
+		CALLOUTS.choose(&mut rand::rng()).unwrap(),
+		voice_channel.mention()
+	));
+	let msg = radon.send_message(ctx, builder).await?;
+
+	// Sleep for 10 minutes then delete
+	tokio::time::sleep(std::time::Duration::from_secs(10 * 60)).await;
+	msg.delete(ctx).await?;
 
 	Ok(())
 }
